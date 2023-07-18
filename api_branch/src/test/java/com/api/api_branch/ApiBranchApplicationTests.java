c051062cd4215d638f1fd25594bf7f142952de09
Luqman Quarshie <kingugly340@gmail.com>
2023-07-18 15:33:02 +0000

    initialized springboot api

---
+++ api_branch/src/test/java/com/api/api_branch/ApiBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.api_branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
