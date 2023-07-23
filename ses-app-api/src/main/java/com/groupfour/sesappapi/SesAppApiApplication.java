3484a3013b653d9342041577a6fecdaa5c3fd094
Serkhani <deviper03@gmail.com>
2023-07-23 08:13:28 +0000

    initialized new spring project

---
+++ ses-app-api/src/main/java/com/groupfour/sesappapi/SesAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.groupfour.sesappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesAppApiApplication.class, args);
+	}
+
+}
---
+++ ses-app-api/src/test/java/com/groupfour/sesappapi/SesAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupfour.sesappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
