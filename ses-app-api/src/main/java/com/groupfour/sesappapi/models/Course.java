28613df67704eb4dd192a85d0945e5d7690dabc6
Serkhani <deviper03@gmail.com>
2023-07-23 14:09:59 +0000

    added course

---
+++ ses-app-api/src/main/java/com/groupfour/sesappapi/models/Course.java
@@ -0,0 +1,28 @@
+package com.groupfour.sesappapi.models;
+
+
+import jakarta.persistence.*;
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long courseId;
+    
+    @Column(nullable = false, unique = true)
+    private String courseName;
+    
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+    
+    @Column(nullable = false)
+    private Integer creditHours;
+    
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    
+    @Column(nullable = false)
+    private String semester;
+    
+    // Constructors, getters, and setters
+}
